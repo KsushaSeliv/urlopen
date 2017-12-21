9ce805356c12966492e2198027d4e24d40d652f3
KsushaSeliv <KsenijaSeliv@yandex.ru>
2017-12-21 19:25:00 +0300

    Create url.py

---
+++ url.py
@@ -0,0 +1,17 @@
+from urllib.request import urlopen 
+import json 
+import pprint 
+
+def i_d(id): 
+ req = "https://api.vk.com/method/users.get?user_ids={id}&v=5.8&fields=online".format(id=id) 
+ try: 
+  req_q = urlopen(req) 
+  q = json.loads(req_q.read()) #чтение Json-данных 
+ except: 
+  print('ERROR') 
+ return q 
+
+if __name__ == "__main__": 
+  id = input('id:') #вводим id нужного пользователя 
+  print('Please:') 
+  pprint.pprint(i_d(id))
