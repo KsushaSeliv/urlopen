1978cbec60947f3940e61af6a94638010b4c1e4d
KsushaSeliv <KsenijaSeliv@yandex.ru>
2018-01-10 15:41:38 +0300

    Update url.py

--- url.py
+++ url.py
@@ -2,11 +2,17 @@ from urllib.request import urlopen
 import json 
 import pprint 
 
-print('Id:') 
-id = input()
-pam = "https://api.vk.com/method/users.get?user_ids={id}&fields=firstname&v=5.69".format(id=id)
-fun = urlopen(pam)
-bj = json.loads(fun.read())
-print(fun)
-print('')
-pprint.pprint(bj)
+def i_d(id): 
+ req = "https://api.vk.com/method/users.get?user_ids={id}&v=5.8&fields=online".format(id=id) 
+ try: 
+  req_q = urlopen(req) 
+  q = json.loads(req_q.read()) #чтение Json-данных 
+ except: 
+  print('ERROR') 
+return q 
+
+if __name__ == "__main__": 
+id = input('id:') #вводим id нужного пользователя 
+print('PLease:') 
+pprint.pprint(i_d(id))
+тут была ошибка я поправил
